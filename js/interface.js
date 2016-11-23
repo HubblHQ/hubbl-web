f1ee7f1fe2eb7110d77c3c81ed59a4ba196d8588
Artem Samokhin <name.didlidu@gmail.com>
2016-11-23 21:29:03 +1000

    working under player

--- js/interface.js
+++ js/interface.js
@@ -1,42 +1,49 @@
+PLAYER_FRAME = '#playlist';
+USER_SETTINGS_FRAME = '#settings';
+
+USERNAME_BTN = '#username-btn';
+BACK_BTN = '#arrow-back-btn';
+MORE_BTN = '#more-btn';
+
+PRELOAD_BAR = '#preload-bar';
+PLAY_BAR = '#play-bar';
+
+class ScreenState {
+
+    constructor(frame) {
+        this.curFrame = frame;
+    }
+
+    getCurFrame() {
+        return this.curFrame;
+    }
+
+    setFrame(frame) {
+
+        switch (frame) {
+            case PLAYER_FRAME:
+                $(BACK_BTN).hide();
+                $(MORE_BTN).show();
+                break;
+            case USER_SETTINGS_FRAME:
+                $(MORE_BTN).hide();
+                $(BACK_BTN).show();
+                break;
+            default:
+                alert('No such frame: ' + frame);
+                return;
+        }
+        $(this.curFrame).hide();
+        this.curFrame = frame;
+        $(this.curFrame).show();
+    }
+}
+
 $(document).ready(function() {
 
-    PLAYER_FRAME = '#playlist';
-    USER_SETTINGS_FRAME = '#settings';
 
-    USERNAME_BTN = '#username-btn';
-    BACK_BTN = '#arrow-back-btn';
-    MORE_BTN = '#more-btn';
-
-    class ScreenState {
-
-        constructor(frame) {
-            this.curFrame = frame;
-        }
-
-        getCurFrame() {
-            return this.curFrame;
-        }
-
-        setFrame(frame) {
-
-            switch (frame) {
-                case PLAYER_FRAME:
-                    $(BACK_BTN).hide();
-                    $(MORE_BTN).show();
-                    break;
-                case USER_SETTINGS_FRAME:
-                    $(MORE_BTN).hide();
-                    $(BACK_BTN).show();
-                    break;
-                default:
-                    alert('No such frame: ' + frame);
-                    return;
-            }
-            $(this.curFrame).hide();
-            this.curFrame = frame;
-            $(this.curFrame).show();
-        }
-    }
+    $(BACK_BTN).hide();
+    $(USER_SETTINGS_FRAME).hide();
 
     var state = new ScreenState(PLAYER_FRAME);
 
